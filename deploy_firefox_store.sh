5f73e59890655fbd9cf19f4c45fb2297292563ac
George L. Yermulnik <yz@yz.kiev.ua>
2023-10-26 22:09:03 +0300

    Adjust to my own needs

--- deploy_firefox_store.sh
+++ deploy_firefox_store.sh
@@ -1,21 +1,22 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+set -e
 
 ENV=production
 OUTPUT_TARGET=firefox
-OUTPUT_FOLDER=./build/$OUTPUT_TARGET/$ENV
-OUTPUT_FILE=$OUTPUT_TARGET.zip
+OUTPUT_FOLDER="./build/$OUTPUT_TARGET/$ENV"
+OUTPUT_FILE="$OUTPUT_TARGET.xpi"
 
-rm -f ./$OUTPUT_FILE
+rm -f "./$OUTPUT_FILE"
 
 yarn
 yarn test-ci
-NODE_ENV=production yarn build-$OUTPUT_TARGET
-cd $OUTPUT_FOLDER
-zip -r ../../../$OUTPUT_FILE ./*
+NODE_ENV=production yarn "build-$OUTPUT_TARGET"
+cd "$OUTPUT_FOLDER"
+zip -r "../../../$OUTPUT_FILE" ./*
 cd -
 
-VERSION=`cat $OUTPUT_FOLDER/manifest.json | json version`
-zip -r firefox_source_$VERSION.zip src utils ./.babelrc package.json README.md webpack.config.js yarn.lock
+#VERSION=$(jq -r .version < "$OUTPUT_FOLDER/manifest.json")
+#zip -r firefox_source_$VERSION.zip src utils ./.babelrc package.json README.md webpack.config.js yarn.lock
 
 echo ""
 echo "Upload: https://addons.mozilla.org/en-US/developers/addons"
